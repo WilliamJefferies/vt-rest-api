328ebf7c52d259a629e577e45887e0c4b10f4775
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-10 13:59:18 +1100

    added todo

--- src/analysisStrategies/index.js
+++ src/analysisStrategies/index.js
@@ -2,3 +2,5 @@ export regionalStrategy from "./regionalStrategy";
 export varietyStrategy from "./varietyStrategy";
 export yearlyStrategy from "./yearlyStrategy";
 export yearVarietyStrategy from "./yearVarietyStrategy";
+
+// todo: reduce to two functions
