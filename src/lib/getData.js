68082bdabb3761865ed863eb6aed9378a7c4d976
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-10 13:25:41 +1100

    process and sort data

---
+++ src/lib/getData.js
@@ -0,0 +1,18 @@
+import * as dataConnector from '../../data'
+import * as lib from '.'
+
+/*
+DB Wrapper
+ */
+export default (lotCode, handleErrParams) => {
+    switch (lotCode.toLowerCase()) {
+        case '11yvchar001':
+            return dataConnector.lot1.components;
+        case '11yvchar002':
+            return dataConnector.lot2.components;
+        case '15mppn002-vk':
+            return dataConnector.lot3.components;
+        default:
+            lib.handleErr(handleErrParams);
+    }
+};
---
+++ src/lib/sortData.js
@@ -0,0 +1,21 @@
+import * as lib from '.'
+
+/*
+Sorts an object into an array of nested percentage values
+could be done better with more time to avoid duplication of memory
+ */
+export default (data, handleErrParams) => {
+    let sortable = [];
+    let returnData = [];
+    for (let item in data) {
+        sortable.push([data[item], data[item].percentage]);
+    }
+    const sorted = sortable.sort(function (a, b) {
+        return b[1] - a[1];
+    });
+
+    for (let item in sorted) {
+        returnData.push(sorted[item][0]);
+    }
+    return returnData;
+};
