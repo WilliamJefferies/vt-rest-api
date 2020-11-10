03d3cf5f9467c4ef609000e06e61349d2af3dfe8
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-10 13:23:10 +1100

    handler for breakdown

---
+++ src/handlers/getBreakdownHandler.js
@@ -0,0 +1,22 @@
+import * as lib from "../lib";
+
+export default (req, res) => {
+    const strategy = req.analysisStrategy;
+    const lotCode = req.lotCode;
+
+    const handleErrParams = {
+        code: 500,
+        message: `500 Internal Server Error:
+    Could not handle breakdown request
+    Strategy: ${strategy}
+    Lot Code: ${lotCode}`,
+        res: res
+    };
+
+    const data = lib.getData(lotCode, handleErrParams);
+    const analysedData = lib.analyseData(data, strategy, handleErrParams);
+    const sortedData = lib.sortData(analysedData, handleErrParams);
+    const returnObj = lib.constructBreakdownReturn(strategy, lotCode, sortedData, handleErrParams);
+
+    res.json(returnObj);
+};
