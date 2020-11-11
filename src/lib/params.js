515e2c2d62fc666b3873f30d4286b91a291bf79c
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-12 09:00:11 +1100

    streamlined param handlers

---
+++ src/lib/params.js
@@ -0,0 +1,35 @@
+import url from 'url';
+import * as lib from '.'
+
+
+// validates params with known strategies and lotcodes
+const validate = (caller_name, param, handleErrVars) => {
+    let validList;
+    caller_name === 'setBreakDownStrategy' ? validList = lib.dataConnector.getValidStrategies()
+        : caller_name === 'setLotCode' ? validList = lib.dataConnector.getValidLotCodes()
+        : lib.handleErr(handleErrVars);
+    if (validList.includes(param)) return true;
+    lib.handleErr(handleErrVars);
+};
+
+// extracts params from req
+const parse = (req, caller_name, handleErrVars) => {
+    const expectedParamsSize = 3;
+    let param;
+
+    const req_url = url.parse(req.url).path;
+    const params = req_url.split('/', expectedParamsSize);
+
+
+    if (!params.length >= expectedParamsSize) {
+        lib.handleErr(handleErrVars);
+    }
+
+    caller_name === 'setBreakDownStrategy' ? param = params[1] // expected strategy param
+        : caller_name === 'setLotCode' ? param = params[2] // expected lot code param
+        : lib.handleErr(handleErrVars)
+
+    return param;
+};
+
+export {parse, validate}
